b2ce3b9f0a2a724e9e6fa078871d5073ac515b45
Alexander Kaikatsishvili <alexanderkaikatsishvili@gmail.com>
2021-06-15 16:38:06 +0300

    Slider adding

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -29,3 +29,18 @@ lightGallery(document.getElementById('lightgallery'), {
   plugins: [lgZoom, lgThumbnail],
   speed: 500,
 });
+
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  focusOnSelect: true,
+});
